78eb835e845f7148c017568948100e64fb0bdc40
eacz <estebanacanteros@gmail.com>
2020-12-15 15:13:45 -0300

    follow/unfollow functionality added on UserListItem

--- src/components/UserListItem.js
+++ src/components/UserListItem.js
@@ -1,10 +1,23 @@
 import React, { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
+import axiosClient from '../config/config';
 import userContext from '../context/userContext/userContext';
 
 const UserListItem = ({ userL }) => {
     const history = useHistory()
-    const { user } = useContext(userContext);
+    const {auth, user, updateUser, } = useContext(userContext);
+
+    
+    const handleFollowUnfollow = async () => {
+        if(!auth) return;
+        const data = { userID: userL._id, follow: !user.following.includes(userL._id) };
+        try {
+            const res = await axiosClient.post('/user/follow', data);
+            updateUser(res.data.user);
+        } catch (error) {
+            console.log(error);
+        }
+    };
     return (
         <div className="userlist-item">
             <div className="left" onClick={() => history.push(`/user/${userL._id}`)}>
@@ -12,13 +25,15 @@ const UserListItem = ({ userL }) => {
                 <p>{userL.username}</p>
             </div>
             <div className="right">
-                {userL._id === user._id ? null : (
+                {userL._id === user._id  && auth ? null : (
                     <button
                         className={`btn  ${
                             user.following.includes(userL._id)
                                 ? 'white black-text btn-f'
                                 : 'blue btn-nf'
-                        }`}
+                        }`
+                    }
+                    onClick={() => handleFollowUnfollow()}
                     >
                         {user.following.includes(userL._id)
                             ? 'Following'
