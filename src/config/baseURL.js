345cd341db4914a4b8e3d4d21b7584440e8f5e37
eacz <estebanacanteros@gmail.com>
2020-12-16 16:47:57 -0300

    production backend URL added

--- src/config/baseURL.js
+++ src/config/baseURL.js
@@ -1,4 +1,4 @@
-const local= "http://localhost:4000"
-//const prod=''
+//const local= "http://localhost:4000"
+const prod='https://immense-castle-27888.herokuapp.com'
 
-export default local
+export default prod
